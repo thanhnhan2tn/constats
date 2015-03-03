e1842dd7163aab0d300096006f0526799bb2314b
thanhnhan2tn <thanhnhan2tn@gmail.com>
2015-03-03 08:15:48 +0700

    Admin Demo

--- Codes/constats/src/main/java/vn/edu/cit/controller/HomeController.java
+++ Codes/constats/src/main/java/vn/edu/cit/controller/HomeController.java
@@ -1,25 +1,13 @@
 package vn.edu.cit.controller;
 
-import java.io.FileNotFoundException;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import vn.edu.cit.model.Server;
 import vn.edu.cit.repositories.UserRepository;
-import vn.edu.cit.servercontrol.Power;
-import vn.edu.cit.servercontrol.nics_controller.Nic;
-import vn.edu.cit.servercontrol.nics_controller.Nics;
 
 /**
  * Handles requests for the application home page.
